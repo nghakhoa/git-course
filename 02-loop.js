457dcb12fc283bb8724d1af94883272aeedfbead
Khoa <khoa1999.81@gmail.com>
2025-05-19 00:26:23 +0700

    feat: remove //

--- 02-loop.js
+++ 02-loop.js
@@ -1,23 +1,22 @@
-// //for (let i = 1; i <= 100; i++);
+for (let i = 1; i <= 100; i++);
+for (let i = 2; i <= 9; i++) {
+    console.log('Bảng cửu chương ' + i);
+    for (let j = 1; j <= 10; j++) {
+        console.log(i + ' x ' + j + ' = ' + (i * j));
+    }
+}
 
-// for (let i = 2; i <= 9; i++) {
-//     console.log('Bảng cửu chương ' + i);
-//     for (let j = 1; j <= 10; j++) {
-//         console.log(i + ' x ' + j + ' = ' + (i * j));
-//     }
-// }
+const soLe = [];
+for (let i = 1; i <= 99; i++) {
+    if (i % 2 === 1) {
+        soLe.push(i);
+    }
+}
+console.log(soLe);
 
-// const soLe = [];
-// for (let i = 1; i <= 99; i++) {
-//     if (i % 2 === 1) {
-//         soLe.push(i);
-//     }
-// }
-// console.log(soLe);
-
-// for (let i = 1; i <= 10; i++) {
-//     console.log('user' + i + 'example.com');
-// }
+for (let i = 1; i <= 10; i++) {
+    console.log('user' + i + 'example.com');
+}
 
 const monthTotal = [
     { month: 1, total: 100 },
